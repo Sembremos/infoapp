93b4d39135352d9babfdffd7a7d3ae03a4c79380
Sembremos <sembremoseguridad@gmail.com>
2026-02-03 10:46:31 -0600

    Update pdf_generator.py

--- pdf_generator.py
+++ pdf_generator.py
@@ -1234,7 +1234,7 @@ def generar_pdf(
             # ================== CUADRO TOTAL ==================
             canvas.setFillColor(colors.HexColor("#013051"))
             canvas.rect(
-                page_width / 2 - 0,
+                page_width / 1,
                 620,
                 200,
                 70,
@@ -1245,7 +1245,7 @@ def generar_pdf(
             canvas.setFillColor(colors.white)
             canvas.setFont("Helvetica-Bold", 16)
             canvas.drawCentredString(
-                page_width / -10,
+                page_width / 1,
                 635,
                 "Total de denuncias"
             )
