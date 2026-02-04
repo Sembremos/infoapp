e253f6ebe1e4a9bace603f564ac8e1b56f41cc30
Sembremos <sembremoseguridad@gmail.com>
2026-02-04 08:47:49 -0600

    Update pdf_generator.py

--- pdf_generator.py
+++ pdf_generator.py
@@ -1245,14 +1245,14 @@ def generar_pdf(
             canvas.setFillColor(colors.white)
             canvas.setFont("Helvetica-Bold", 12)
             canvas.drawCentredString(
-                page_width / 2 + 165,
+                page_width / 2 + 175,
                 660,
                 "Total de denuncias"
             )
         
             canvas.setFont("Helvetica-Bold", 22)
             canvas.drawCentredString(
-                page_width / 2 + 165,
+                page_width / 2 + 175,
                 675,
                 str(total_denuncias)
             )
