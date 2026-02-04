da794ed91ffbbe98a750ced11fe0d558f0542814
Sembremos <sembremoseguridad@gmail.com>
2026-02-04 17:23:03 -0600

    Update pdf_generator.py

--- pdf_generator.py
+++ pdf_generator.py
@@ -1295,22 +1295,22 @@ def generar_pdf(
             
             canvas.setFillColor(colors.white)
             canvas.setFont("Helvetica-Bold", 12)
-            canvas.drawCentredString(320, 400, "AM")
+            canvas.drawCentredString(320, 350, "AM")
             
             canvas.setFont("Helvetica-Bold", 18)
-            canvas.drawCentredString(320, 380, str(total_am))
+            canvas.drawCentredString(320, 330, str(total_am))
             
             
             # ===== CUADRO PM =====
             canvas.setFillColor(colors.HexColor("#013051"))
-            canvas.rect(280, 420, 100, 40, fill=1, stroke=0)
+            canvas.rect(280, 300, 100, 40, fill=1, stroke=0)
             
             canvas.setFillColor(colors.white)
             canvas.setFont("Helvetica-Bold", 12)
-            canvas.drawCentredString(320, 430, "PM")
+            canvas.drawCentredString(320, 290, "PM")
             
             canvas.setFont("Helvetica-Bold", 18)
-            canvas.drawCentredString(320, 440, str(total_pm))
+            canvas.drawCentredString(320, 270, str(total_pm))
 
             ##tabla grande
             draw_tabla_simple(
