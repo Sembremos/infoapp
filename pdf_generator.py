9720ce924b8871a863ce7881d2761c350dab366b
Sembremos <sembremoseguridad@gmail.com>
2026-02-28 13:10:43 -0600

    Update pdf_generator.py

--- pdf_generator.py
+++ pdf_generator.py
@@ -1555,11 +1555,11 @@ def draw_pagina_percepcion_2(
     GRAFICO_X = 40
 
     TABLA_X = 40
-    TABLA_Y = 130
+    TABLA_Y = 200
     TABLA_WIDTH = page_width * 0.55
 
     TEXTO_X = page_width * 0.65
-    TEXTO_Y = 260
+    TEXTO_Y = 150
 
     # =================================================
 
